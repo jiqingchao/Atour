2b1498cba6038abbce7bb763c7190aa68a1bf70f
jiqingchao <jiqingchao@kanfanews.com>
2020-02-28 22:28:37 +0800

    system

--- complete/src/main/java/hello/Greeter2.java
+++ complete/src/main/java/hello/Greeter2.java
@@ -2,8 +2,8 @@ package hello;
 
 public class Greeter2 {
 	public String sayHello() {
-		system.out.printf("hello");
-		system.out.printf("hello");
+		System.out.printf("hello");
+		System.out.printf("hello");
 		return "Hello world!";
 	}
 }
